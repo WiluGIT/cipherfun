2a0385f7b769cdd270bde6dc009ebebc784baf3e
Konrad Wilczek <wolframowsky@gmail.com>
2020-03-14 16:32:42 +0100

    transposition cipher with intermediate mix finished

--- przestawieniamacierzowe2c.py
+++ przestawieniamacierzowe2c.py
@@ -9,6 +9,7 @@ def print_matrix(matrix):
 def find_char(s, ch):
     return [i for i, ltr in enumerate(s) if ltr == ch]
 
+
 def get_column(matrix,col,row_length):
     result = []
     for i in range(row_length):
@@ -21,9 +22,81 @@ def get_column(matrix,col,row_length):
     return "".join(result)
 
 
+def decode():
+    text = "HEESPNIRRSSEESEIYASCBTEMGEPNANDICTRTAHSOIEERO".replace(" ", "").upper()
+    key = "CONVENIENCE".upper()
+
+    _dic = {}
+    for i in range(len(key)):
+        counter = 1
+        for j in range(i + 1, len(key)):
+            if key[i] == key[j]:
+                counter += 1
+        if _dic.get(key[i]) == None:
+            _dic[key[i]] = counter
+
+    # sort dic
+    sort_dic = {}
+    for i in sorted(_dic):
+        sort_dic.update({i: _dic[i]})
+
+
+    col = len(key)
+
+    row_values = []
+    txt_pointer = 0
+    for i in sort_dic:
+        col_array = find_char(key, i)
+        for j in range(len(col_array)):
+            if txt_pointer < len(text):
+                if col_array[j] == 0:
+                    row_values.append(text[txt_pointer:1])
+                    txt_pointer += 1
+                else:
+                    row_values.append(text[txt_pointer:txt_pointer + col_array[j] + 1])
+                    txt_pointer += col_array[j] + 1
+
+    row_count = len(row_values)
+    print("Row count {}".format(row_count))
+    # create matrix and populate while we know how many rows it has
+    matrix = [["x" for i in range(col)] for i in range(row_count)]
+
+    #going through matrix and place - where there should be chatacter
+
+    for i in range(row_count):
+        row_elem = len(row_values[i])
+        for j in range(col):
+            if j < row_elem:
+                matrix[i][j] = "-"
+
+    print_matrix(matrix)
+
+    # insert cipher by columns in - place
+    txt_ptr = 0
+    for i in sort_dic:
+        col_array = find_char(key, i)
+        for j in range(len(col_array)):
+            for k in range(row_count):
+                if matrix[k][col_array[j]] == "-":
+                    matrix[k][col_array[j]] = text[txt_ptr]
+                    txt_ptr += 1
+
+    print("\n")
+    print_matrix(matrix)
+
+    result = []
+    for i in range(row_count):
+        for j in range(col):
+            if matrix[i][j] != "x":
+                result.append(matrix[i][j])
+
+    final_result = "".join(result)
+    print("Decoded result:\n{}".format(final_result))
+
+
 def encode():
-    text = "CHUJUZAJEBANYBEKAZCIEBIEBECZUNIA".replace(" ", "").upper()
-    key = "CHUJOSTWO".upper()
+    text = "HERE IS A SECRET MESSAGE ENCIPHERED BY TRANSPOSITION".replace(" ", "").upper()
+    key = "CONVENIENCE".upper()
 
     # get number of occurance in key to dictionary
     _dic = {}
@@ -40,8 +113,6 @@ def encode():
     for i in sorted(_dic):
         sort_dic.update({i: _dic[i]})
 
-    print(sort_dic)
-
     # create matrix
     col = len(key)
     text_len = float(len(text))
@@ -53,7 +124,6 @@ def encode():
     # take rows and move through text with txt_pointer and append text within position of key chars
     for i in sort_dic:
         col_array = find_char(key, i)
-        print(col_array)
         for j in range(len(col_array)):
             if txt_pointer < len(text):
                 if col_array[j] == 0:
@@ -67,13 +137,10 @@ def encode():
 
     # create matrix and populate while we know how many rows it has
     matrix = [["-" for i in range(col)] for i in range(row_count)]
-    print_matrix(matrix)
-    print(row_values)
+
     for i in range(row_count):
         for j in range(col):
             matrix[i] = row_values[i]
-
-
 
     print_matrix(matrix)
     result = []
@@ -84,4 +151,6 @@ def encode():
     print("Encoded result:")
     print("".join(result))
 
+
 encode()
+decode()
