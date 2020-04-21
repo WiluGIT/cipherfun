50148894c352167a0d22e57a8f09836c60414df8
wolframowsky <wolframowsky@gmail.com>
2020-04-21 22:11:21 +0200

    synchronous cipher decode/encode

--- LFSR.py
+++ LFSR.py
@@ -1,42 +1,55 @@
-# def lfsr2(seed, taps):
-#     sr = seed
-#     nbits = 3
-#     while 1:
-#         xor = 1
-#         for t in taps:
-#             if (sr & (1<<(t-1))) != 0:
-#                 xor ^= 1
-#         sr = (xor << nbits-1) + (sr >> 1)
-#         yield xor, sr
-#         if sr == seed:
-#             break
-#
-# nbits = 3
-# for xor, sr in lfsr2(0b001, (1,3)):
-#     print(xor, bin(2**nbits+sr)[3:])
-#
-# print("gowno", 0b001>>1)
-
-
-
 def lfsr(seed, taps):
-    result = seed
+    state = seed
     xor = 0
     while 1:
         for t in taps:
-            xor += int(result[t-1])
+            xor += int(state[t-1])
         if xor % 2 == 0.0:
             xor = 0
         else:
             xor = 1
 
-        result = str(xor) + result[:-1]
+        output = state[-1]
+        state = str(xor) + state[:-1]
 
-        yield result, xor
+        yield state, output, xor
         xor = 0
-        if result == seed:
+        if state == seed:
             break
 
-print("state    xor")
-for state, xor in lfsr('10000', (3,5)):
-    print("{}   {}".format(state, xor))
+
+def synchronous_cipher_encode(plaintext, seed, taps):
+    lfsr_xor = ""
+    for state, output, xor in lfsr(seed, taps):
+        lfsr_xor += str(xor)
+
+    encoded_result = ""
+    for i in range(len(plaintext)):
+        result = str(int(lfsr_xor[i]) ^ int(plaintext[i]))
+        encoded_result += result
+
+    print("Encoded result:\n{}".format(encoded_result))
+
+
+def synchronous_cipher_decode(ciphertext, seed, taps):
+    lfsr_xor = ""
+    for state, output, xor in lfsr(seed, taps):
+        lfsr_xor += str(xor)
+
+    decoded_result = ""
+    for i in range(len(ciphertext)):
+        result = str(int(lfsr_xor[i]) ^ int(ciphertext[i]))
+        decoded_result += result
+
+    print("Decoded result:\n{}".format(decoded_result))
+
+
+plaintext ="101011111100001"
+ciphertext = "110101010000101"
+seed = "0010"
+taps = (1,4)
+synchronous_cipher_encode(plaintext,seed,taps)
+synchronous_cipher_decode(ciphertext,seed,taps)
+# print("state    output      xor")
+# for state, output, xor in lfsr('1000', (1,4)):
+#     print("{}      {}           {}".format(state, output,xor))
