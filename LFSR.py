2e05206a044204c7f04b936315519200b243034f
wolframowsky <wolframowsky@gmail.com>
2020-04-22 15:48:11 +0200

    Finished autokey_cipher encode/decode

--- LFSR.py
+++ LFSR.py
@@ -28,7 +28,7 @@ def synchronous_cipher_encode(plaintext, seed, taps):
         result = str(int(lfsr_xor[i]) ^ int(plaintext[i]))
         encoded_result += result
 
-    print("Encoded result:\n{}".format(encoded_result))
+    print("Encoded synchronous result:\n{}".format(encoded_result))
 
 
 def synchronous_cipher_decode(ciphertext, seed, taps):
@@ -41,15 +41,62 @@ def synchronous_cipher_decode(ciphertext, seed, taps):
         result = str(int(lfsr_xor[i]) ^ int(ciphertext[i]))
         decoded_result += result
 
-    print("Decoded result:\n{}".format(decoded_result))
+    print("Decoded synchronous result:\n{}".format(decoded_result))
+
+def autokey_cipher_encode(plaintext, seed, taps):
+    state = seed
+    xor = 0
+    counter = 0
+    ciphertext_result = ""
+    while 1:
+        for t in taps:
+            xor += int(state[t - 1])
+        if xor % 2 == 0.0:
+            xor = 0
+        else:
+            xor = 1
+
+        z_x_xor = str(int(plaintext[counter]) ^ int(xor))
+        state = z_x_xor + state[:-1]
+        counter += 1
+        ciphertext_result += z_x_xor
+        xor = 0
+        if counter >= len(plaintext):
+            break
+    print("Encoded autokey result:\n{}".format(ciphertext_result))
 
 
-plaintext ="101011111100001"
-ciphertext = "110101010000101"
-seed = "0010"
+def autokey_cipher_decode(ciphertext, seed, taps):
+    state = seed
+    xor = 0
+    counter = 0
+    plaintext_result = ""
+    while 1:
+        for t in taps:
+            xor += int(state[t - 1])
+        if xor % 2 == 0.0:
+            xor = 0
+        else:
+            xor = 1
+
+        current_bit = ciphertext[counter]
+        y_z_xor = str(int(current_bit) ^ int(xor))
+        state = current_bit + state[:-1]
+        plaintext_result += y_z_xor
+        counter += 1
+        xor = 0
+        if counter >= len(ciphertext):
+            break
+    print("Decoded autokey result:\n{}".format(plaintext_result))
+
+
+
+plaintext = "11101001"
+ciphertext_sync = "01100110"
+ciphertext_autokey = "00110011"
+seed = "0011"
 taps = (1,4)
-synchronous_cipher_encode(plaintext,seed,taps)
-synchronous_cipher_decode(ciphertext,seed,taps)
-# print("state    output      xor")
-# for state, output, xor in lfsr('1000', (1,4)):
-#     print("{}      {}           {}".format(state, output,xor))
+synchronous_cipher_encode(plaintext, seed, taps)
+synchronous_cipher_decode(ciphertext_sync, seed, taps)
+autokey_cipher_encode(plaintext, seed, taps)
+autokey_cipher_decode(ciphertext_autokey, seed, taps)
